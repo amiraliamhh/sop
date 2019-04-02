8db91c4aed9652549986f46f3b4896af1c33b659
amirali <amiraliamhh@gmail.com>
2019-04-02 04:45:01 +0430

    test endpoint

--- src/Containers/App.tsx
+++ src/Containers/App.tsx
@@ -1,3 +1,50 @@
 import * as React from 'react'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider, Query } from 'react-apollo'
+import gql from 'graphql-tag'
 
-export default () => <p>Hello!</p>
+const client = new ApolloClient({
+    uri: "https://api.github.com/graphql",
+    request: async operation => {
+        operation.setContext({
+            headers: {
+                "Authorization": `bearer ${process.env.GITHUB_PERSONAL_TOKEN}`,
+            },
+        });
+    },
+})
+
+const q = gql`
+    query {
+        repository(owner: "amiraliamhh", name: "sop") {
+            collaborators(first: 1) {
+                nodes {
+                    bio
+                }
+            }
+        }
+}
+`
+
+export default () => (
+    <ApolloProvider client={client} >
+        <h1>Github!</h1>
+        <Query query={q} >
+        {
+            ({ data, loading }) => {
+                console.log(data)
+
+                return (
+                    <div>
+                        {
+                        loading
+                        ? <p>loading ...</p>
+                        : <p>loaded</p>
+                        } 
+                    </div>
+                )
+            }
+        }
+        </Query>
+    </ApolloProvider>
+)
